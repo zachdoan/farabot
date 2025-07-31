417db1831f76461771cd7b15ede3c211ac4c92b2
zachdoan <zachhdoan@gmail.com>
2025-07-31 15:36:12 +0700

    whitelist and cleanup

--- src/commands/trialprojectteams.js
+++
@@ -1,8 +0,0 @@
-const teams = require('../teams');
-
-module.exports = {
-    name: 'trialprojectteams',
-    async execute(message, args, client) {
-        message.channel.send('operational');
-    }
-}
